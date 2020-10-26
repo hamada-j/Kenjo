cd5856833b60217d1995b35afad2012881a02bfa
Hamada <defjamvk@gmail.com>
2020-10-27 00:19:37 +0100

    Commit transit
    Commit transit

--- Kenjo/src/app/landing page/landingpage.component.spec.ts
+++ Kenjo/src/app/landing page/landingpage.component.spec.ts
@@ -1,25 +1,41 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject, tick, fakeAsync } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { LandingPageComponent } from './landingpage.component';
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
   let fixture: ComponentFixture<LandingPageComponent>;
+  let de: DebugElement;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LandingPageComponent ]
-    })
-    .compileComponents();
-  }));
+      TestBed.configureTestingModule({
+        declarations: [ LandingPageComponent ]
+      })
+      .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LandingPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+
+
+  // let component: LandingPageComponent;
+  // let fixture: ComponentFixture<LandingPageComponent>;
+
+  // beforeEach(async(() => {
+  //   TestBed.configureTestingModule({
+  //     declarations: [ LandingPageComponent ]
+  //   })
+  //   .compileComponents();
+  // }));
+
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(LandingPageComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
+
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
 });
--- Kenjo/src/app/pipe/pipe-image.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PipeImagePipe } from './pipe-image.pipe';
-
-describe('PipeImagePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PipeImagePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
